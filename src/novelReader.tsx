3461ec2f23d4b868a7202d061d904fe056ec1c13
ToYama <toyamasoujinn@gmail.com>
2025-01-19 17:36:22 +0000

    feat: 基本的なコンポーネントを作成

---
+++ src/novelReader.tsx
@@ -0,0 +1,50 @@
+import React, { JSX, RefObject, useRef } from "react";
+
+const readerContext =
+  React.createContext<RefObject<HTMLDivElement | null> | null>(null);
+
+type ComponentPropsWithClassName<
+  T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<unknown>,
+> = {
+  className?: string;
+} & React.ComponentProps<T>;
+
+export const Root: React.FC<ComponentPropsWithClassName<"div">> = (props) => {
+  const viewer = useRef<HTMLDivElement>(null);
+  return (
+    <readerContext.Provider value={viewer}>
+      {props.children}
+    </readerContext.Provider>
+  );
+};
+
+export const Viewer: React.FC<ComponentPropsWithClassName<"div">> = (props) => {
+  const viewer = React.useContext(readerContext);
+  return (
+    <div {...props} ref={viewer}>
+      {props.children}
+    </div>
+  );
+};
+
+export const NextButton: React.FC<ComponentPropsWithClassName<"button">> = (
+  props
+) => {
+  const viewer = React.useContext(readerContext);
+  return (
+    <button {...props} onClick={() => console.log(viewer)}>
+      {props.children}
+    </button>
+  );
+};
+
+export const PrevButton: React.FC<ComponentPropsWithClassName<"button">> = (
+  props
+) => {
+  const viewer = React.useContext(readerContext);
+  return (
+    <button {...props} onClick={() => console.log(viewer)}>
+      {props.children}
+    </button>
+  );
+};
